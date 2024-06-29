8a866d490600eedba444001c5c91ae72002f8445
mirinai <tmddjs1771@gmail.com>
2024-06-29 16:24:16 +0900

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+("./Category/Tabs");
+import CategoryButton from "./Category/CategoryButton";
+import Counter from "./musinsa/Counter";
+import Tabs from "./Category/Tabs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <p className="text-red-600">dasfjkalsdfgjalsdfk</p>
+      <p className="font-bold">asdf</p> */}
+      <>
+        <Tabs />
+        {/* <CategoryButton image="watcha.png" title="watcha" />
+        <CategoryButton image="netflix.png" title="watcha" />
+        <CategoryButton image="disney.jpg" title="watcha" /> */}
+      </>
+      <>
+        <Counter price={10000} />
+        <Counter price={20000} />
+        <Counter price={4000} />
+      </>
+      {/* <>
+        <CategoryButton image="watcha.png" title="watcha" />
+        <CategoryButton image="netflix.png" title="netflix" />
+        <CategoryButton image="disney.jpg" title="disney" />
+      </> */}
     </div>
   );
 }
