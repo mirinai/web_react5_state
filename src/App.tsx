f105167bb05fb57be0fb51b93a699753155e0e0e
mirinai <tmddjs1771@gmail.com>
2024-06-30 15:52:18 +0900

    git add .!

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,39 @@
 import CategoryButton from "./Category/CategoryButton";
 import Counter from "./musinsa/Counter";
 import Tabs from "./Category/Tabs";
+import Snapshot from "./Snapshot";
+import Effect from "./Effect";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Musinsa from "./musinsa/Musinsa";
+
+import Home from "./Home";
+import Product from "./Product";
+import Yeoshin from "./yeoshin/Yeoshin";
+import Layout from "./yeoshin/Layout";
 
 function App() {
   return (
     <div className="App">
+      <Layout />
+      <div>-------------------------</div>
+      <Yeoshin />
+      <div>-------------------------</div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="" Component={Home}></Route>
+          <Route path="/Category" Component={Tabs}></Route>
+          <Route path="/musinsa" Component={Musinsa}></Route>
+          <Route path="/Product/:abc" Component={Product}></Route>
+        </Routes>
+      </BrowserRouter>
+
+      <div>-------------------------</div>
+
+      <Effect />
+      <div>-------------------------</div>
+      <Snapshot />
+
       {/* <p className="text-red-600">dasfjkalsdfgjalsdfk</p>
       <p className="font-bold">asdf</p> */}
       <>
